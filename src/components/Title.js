f7ac955702abe46393d6715bed5aa85f5fa80df7
markofilipovic2762 <markofilipovicsd@gmail.com>
2020-12-08 22:34:18 +0100

    some changes

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ const Title = () => {
     return (
         <div className="title">
             <h1>GalleryApp</h1>
-            <h2>MS Odeca</h2>
-            <p>Najlepsa zenska odeca samo za Vas </p>
+            <h2>Upload pictures for free</h2>
+            <p>The most beautiful pictures on web, you can find on <strong>GalleryApp.com</strong></p>
         </div>
     );
 };
